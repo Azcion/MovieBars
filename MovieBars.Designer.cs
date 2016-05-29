adf823151c8034227e4014ac8c6a6dfcf8332015
Azcion <zak.klajderic@gmail.com>
2016-05-29 14:52:28 +0200

    Init

---
+++ MovieBars.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace MovieBars {
+	partial class Window {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose (bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent () {
+			this.richTextBox = new System.Windows.Forms.RichTextBox();
+			this.SuspendLayout();
+			// 
+			// richTextBox
+			// 
+			this.richTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.richTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.richTextBox.Font = new System.Drawing.Font("Calibri", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.richTextBox.Location = new System.Drawing.Point(12, 12);
+			this.richTextBox.Name = "richTextBox";
+			this.richTextBox.ReadOnly = true;
+			this.richTextBox.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.None;
+			this.richTextBox.Size = new System.Drawing.Size(376, 49);
+			this.richTextBox.TabIndex = 0;
+			this.richTextBox.Text = "Processing in progress...";
+			// 
+			// window
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(400, 73);
+			this.Controls.Add(this.richTextBox);
+			this.Name = "window";
+			this.Text = "MovieBars";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.RichTextBox richTextBox;
+	}
+}
+
---
+++ MovieBars.cs
@@ -0,0 +1,87 @@
+﻿using AForge.Video.FFMPEG;
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+
+
+namespace MovieBars {
+
+	public partial class Window : Form {
+
+		Bitmap bars;
+		int currentX;
+		int frameCount;
+		int barsHeight = 500;
+		int frameDrops = 23;
+
+
+		public Window () {
+			InitializeComponent();
+
+			ProcessVideo("test.avi");
+			bars.Save("out.png");
+
+			richTextBox.Text = "Processing complete.";
+		}
+
+		private void ProcessVideo (string file) {
+			VideoFileReader reader = new VideoFileReader();
+			reader.Open(file);
+			frameCount = (int) reader.FrameCount;
+			bars = new Bitmap(frameCount / (frameDrops + 1), barsHeight);
+
+			for (int i = 0; i < frameCount / (frameDrops + 1); ++i) {
+				for (int j = 0; j < frameDrops; ++j) {
+					try {
+						Bitmap drop = reader.ReadVideoFrame();
+						drop.Dispose();
+					} catch (ArgumentException e) {
+						return;
+					} 
+				}
+				Bitmap frame = reader.ReadVideoFrame();
+				DrawAveragePixel(ref frame);
+				frame.Dispose();
+			}
+        }
+
+		private void DrawAveragePixel (ref Bitmap bm) {
+			BitmapData srcData = bm.LockBits(
+					new Rectangle(0, 0, bm.Width, bm.Height),
+					ImageLockMode.ReadOnly,
+					PixelFormat.Format32bppArgb);
+
+			int stride = srcData.Stride;
+			IntPtr Scan0 = srcData.Scan0;
+			long[] totals = new long[] {0, 0, 0};
+			int width = bm.Width;
+			int height = bm.Height;
+
+			unsafe {
+				byte* p = (byte*) (void*) Scan0;
+
+				for (int y = 0; y < height; ++y) {
+					for (int x = 0; x < width; ++x) {
+						for (int color = 0; color < 3; ++color) {
+							int idx = (y * stride) + x*4 + color;
+							totals[color] += p[idx];
+						}
+					}
+				}
+			}
+
+			int avgR = (int) totals[2] / (width * height);
+			int avgG = (int) totals[1] / (width * height);
+			int avgB = (int) totals[0] / (width * height);
+
+			using (Graphics graphics = Graphics.FromImage(bars)) {
+				Pen pen = new Pen(Color.FromArgb(avgR, avgG, avgB));
+				graphics.DrawLine(pen, currentX, 0, currentX, barsHeight);
+				currentX++;
+			}
+
+			bm.UnlockBits(srcData);
+		}
+	}
+}
